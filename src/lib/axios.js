9aca1e7b40e4106277c16b19b780ddd459cbca16
turab202 <turabturqb@gmail.com>
2025-07-22 16:43:50 +0300

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,4 +5,16 @@ const axiosInstance = axios.create({
   withCredentials: false,
 });
 
+// Add interceptor to attach token on every request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default axiosInstance;
